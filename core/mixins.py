f8f03c8db3924bf28faa3d0208232812211fc278
Samir <54107548+sam1rrrr@users.noreply.github.com>
2026-01-13 18:05:38 +0300

    .

--- core/mixins.py
+++ core/mixins.py
@@ -3,4 +3,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 class OwnerRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
     def test_func(self):
         obj = self.get_object()
+
+        if hasattr(obj, 'restaurant'):
+            return obj.restaurant.owner == self.request.user or self.request.user.is_superuser
+        
         return obj.owner == self.request.user
